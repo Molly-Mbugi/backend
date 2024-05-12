c18be342cf76750b0a65128f87ae91d2c31f39b3
molly <mollynzembi@gmail.com>
2024-05-12 12:53:22 +0300

    port

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000; // Set the default port to 4000
@@ -6,9 +7,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => { // Explicitly bind to the 0.0.0.0 address
   console.log(`Server is running on port ${port}`);
 });
-
-
-
