211467b9b4a812f603a9378e91531387025e728f
molly <mollynzembi@gmail.com>
2024-05-12 12:17:10 +0300

    ports

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ app.get('/data', (req, res) => {
   res.sendFile(path.join(__dirname, 'db.json'));
 });
 
+// Serve db.json file when users access the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'db.json'));
+});
+
 // Define a port to listen on
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
