03f58a7392205eba1dc2c276d887a5b84d6bb7fa
molly <mollynzembi@gmail.com>
2024-05-12 21:40:55 +0300

    node

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000; // Change the port if needed
 
 // Define a route handler to serve db.json
 app.get('/data', (req, res) => {
@@ -17,6 +17,22 @@ app.get('/data', (req, res) => {
   });
 });
 
+// Define a route handler to serve episodes
+app.get('/episodes', (req, res) => {
+  const dbPath = path.join(__dirname, 'db.json');
+  fs.readFile(dbPath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading db.json:', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    // Assuming your JSON data structure has episodes field
+    const episodes = JSON.parse(data).episodes;
+    res.json(episodes);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
